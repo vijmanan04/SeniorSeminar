e17c26e847b750468d990a63bd94b5d2d9e8981e
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2022-03-07 11:07:02 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Student{
+	private String lastName;
+	private String firstName;
+	private int[] choices;
+	public Student(String[] info){
+		this.choices = new int[info.length - 2]; // everything except first 2 indexes are part of student choices
+		this.lastName = info[0];
+		this.firstName = info[1];
+		
+		for (int i = 0; i < info.length - 2; i++){
+			this.choices[i] = Integer.parseInt(info[i+2]); // covnvert each choice to an int for easier comparison later 
+		}
+	}
+}
