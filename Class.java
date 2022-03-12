e26428ebac76dc971ec2594c5bb3a26bb2c555a1
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2022-03-12 13:27:55 -0500

    Update Class.java

--- Class.java
+++ Class.java
@@ -1,20 +1,27 @@
-
 import java.util.ArrayList;
 import java.util.Arrays;
 public class Class {
-	
-	ArrayList<Student> students = new ArrayList<Student>();
-	
+
+	private ArrayList<Student> students = new ArrayList<Student>();
+	private ArrayList<ArrayList<String>> sessions = new ArrayList<ArrayList<String>>();
+
 	public void addStudent(Student s1){ // adds a student to the class
 		students.add(s1);
 	}
-	
+
+	public void addSession(ArrayList<String> s1){
+		sessions.add(s1);
+	}
+
 	public void showClass(){ // prints all Students in the student arrayList to show how many students are in the arrayList
 		for (Student student : students){
-			System.out.println(student);
+			System.out.println(student.toString());
+		}
+	}
+
+	public void showSessions(){ // prints all Sessions in the session arrayList to show how many sessions are in the arrayList
+		for (ArrayList<String> session : sessions){
+			System.out.println(session);
 		}
 	}
 }
-	
-
-
