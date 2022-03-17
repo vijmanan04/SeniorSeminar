f84fdcec6694ab07eea1553fea1b71663a7cfc65
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2022-03-16 22:40:39 -0400

    Update Class.java

--- Class.java
+++ Class.java
@@ -3,14 +3,17 @@ import java.util.Arrays;
 public class Class {
 
 	private ArrayList<Student> students = new ArrayList<Student>();
-	private ArrayList<Speaker> sessions = new ArrayList<Speaker>();
+	private ArrayList<Speaker> speakers = new ArrayList<Speaker>();
+	private ArrayList<Integer> sessions = new ArrayList<Integer>();
+	private Permute permuter = new Permute();
 
 	public void addStudent(Student s1){ // adds a student to the class
 		students.add(s1);
 	}
 
 	public void addSession(Speaker s1){
-		sessions.add(s1);
+		sessions.add(s1.getSessionID());
+		speakers.add(s1);
 	}
 
 	public void showClass(){ // prints all Students in the student arrayList to show how many students are in the arrayList
@@ -20,22 +23,63 @@ public class Class {
 	}
 
 	public void showSessions(){ // prints all Sessions in the session arrayList to show how many sessions are in the arrayList
-		for (Speaker session : sessions){
-			System.out.println(session.toString());
+		for (Speaker speaker : speakers){
+			System.out.println(speaker.toString());
 		}
 	}
-	
+
 	public void makeSessions(){
 		int[] sessionPopularity = new int[18];
 		for (int i = 0; i < students.size(); i++){
 			int[] arr = students.get(i).getChoices();
-			for (int j = 0; i < arr.length; j++){
+			for (int j = 0; j < arr.length; j++){
 				sessionPopularity[arr[j] - 1] = sessionPopularity[arr[j] - 1] + 1;
 			}
 		}
-		
+
 		for (int val : sessionPopularity){
-			System.out.print(val);
+			System.out.print(val + " ");
 		}
+
+
+		ArrayList<ArrayList<Student>> preferredStudents = new ArrayList<ArrayList<Student>>();
+		for (int i = 0 ; i < sessionPopularity.length; i++){
+
+			preferredStudents.add(new ArrayList<Student>());
+			for (Student student : students){
+				int[] choices = student.getChoices();
+				for (int val : choices){
+					if (val == i + 1){
+						preferredStudents.get(i).add(student);
+					}
+				}
+
+				if (preferredStudents.get(i).size() > 30){
+					break;
+				}
+			}
+		}
+
+		ArrayList<Integer> repeats = new ArrayList<Integer>();
+
+		int previousMax = 100000;
+		for (int i = 0; i < 12; i++){
+			int currMax = 0;
+			for (int j = 0; j < sessionPopularity.length; j++){
+				if (sessionPopularity[i] > currMax && currMax < previousMax){
+					currMax = sessionPopularity[i];
+					repeats.add(i + 1);
+				}
+			}
+			previousMax = currMax;
+		}
+
+		sessions.addAll(repeats);
+
+
+
+
+
+
 	}
 }
