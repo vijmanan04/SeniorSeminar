390de37e9422546c847ca22e41c4da9c82e1f0e4
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2022-03-16 22:40:55 -0400

    Update Speaker.java

--- Speaker.java
+++ Speaker.java
@@ -2,14 +2,18 @@ import java.util.ArrayList;
 public class Speaker{
 	private String lastName;
 	private String firstName;
-	private String sessionID;
+	private int sessionID;
 	public Speaker(String[] info){
-		this.sessionID = info[0]; 
+		this.sessionID = Integer.parseInt(info[0]);
 		this.lastName = info[2];
 		this.firstName = info[1];
 	}
-	
+
 	public String toString(){ // add toString to get information on a student, not just pointer in memory when printing
 		return "First Name: " + this.firstName + " Last Name: " + this.lastName + " Session ID: " + this.sessionID;
 	}
+
+  public int getSessionID(){
+    return sessionID;
+  }
 }
